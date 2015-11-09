577eb94f5263ae78c7c26c46a71ef34aea3e17c3
Glowbal <thomasskooi@live.nl>
2015-11-09 09:56:37 +0100

    Add editor config

---
+++ partitions/app.js
@@ -0,0 +1,13 @@
+
+
+
+var arr = [];
+var orgNumb = 5;
+var substracted = 0;
+
+while (orgNumb - substracted > 0) {
+  substracted++;
+  var entry = [orgNumb-substracted];
+  arr.push(entry);
+  
+}
