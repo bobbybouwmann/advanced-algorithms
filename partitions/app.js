476f3c3a8e4e4cc0c5176c0f0f49afa2a8e5de9e
Glowbal <thomasskooi@live.nl>
2015-11-09 11:24:01 +0100

    Add working implementation

--- partitions/app.js
+++ partitions/app.js
@@ -1,13 +1,31 @@
+// for each number,
+// find the possible combinations that can be found
+// ie
+//  5: 4 + 1
+//  5: 3 + 1 + 1
+//  5: 3 + 2
+//  5: 2 + 1 + 2
+//  5: 2 + 1 + 1 +1
+//  5: 2 + 3
+//  5: 1 + 1 + 1 + 1 + 1
+function getPartitions(number) {
+    if (number == 1) {
+        return 1;
+    }
 
+    var possibilies = [];
+    for (var i = number; i > 0; i--) {
+        for (var j = 1; j < number-i+1 && j <= i; j++) {
+            // Starting point of each combination is always i
+            var entry = [i, j];
 
-
-var arr = [];
-var orgNumb = 5;
-var substracted = 0;
-
-while (orgNumb - substracted > 0) {
-  substracted++;
-  var entry = [orgNumb-substracted];
-  arr.push(entry);
-  
+            while (entry.reduce(function(a, b){return a+b;}) != number) {
+                entry.push(1);
+            }
+            possibilies.push(entry);
+        }
+    }
+    return possibilies;
 }
+
+console.log(getPartitions(5));
